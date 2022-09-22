ff12a6d184675c88d16e6f2c172a600d3a6ce113
luzhijian <luzhijian@wodcloud.com>
2022-09-22 14:24:07 +0800

    修改driver名称

--- scm/driver/beagle/beagle.go
+++ scm/driver/beagle/beagle.go
@@ -41,7 +41,7 @@ func New(uri string) (*scm.Client, error) {
 	client := &wrapper{new(scm.Client)}
 	client.BaseURL = base
 	// initialize services
-	client.Driver = scm.DriverOther
+	client.Driver = scm.DriverBeagle
 	// client.Linker = &linker{base.String()}
 	client.Contents = &contentService{client}
 	// client.Git = &gitService{client}
