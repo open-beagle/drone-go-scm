7e2ba1d62b32b4e1b16c37bad2ab06ff5d0f95c3
luzhijian <luzhijian@wodcloud.com>
2023-04-10 19:01:48 +0800

    🐞 fix: webhook

--- scm/driver/beagle/webhook.go
+++ scm/driver/beagle/webhook.go
@@ -24,10 +24,37 @@ func (s *webhookService) Parse(req *http.Request, fn scm.SecretFunc) (scm.Webhoo
 	if err != nil {
 		return nil, err
 	}
+
 	var hook scm.Webhook
-	err = json.Unmarshal(data, &hook)
-	if err != nil {
-		return hook, err
+
+	for {
+		hook, err = convertPushHook(data)
+		if err == nil {
+			break
+		}
+		hook, err = converBranchHook(data)
+		if err == nil {
+			break
+		}
+		hook, err = convertCommentHook(data)
+		if err == nil {
+			break
+		}
+		hook, err = convertTagHook(data)
+		if err == nil {
+			break
+		}
+		hook, err = convertPullRequestHook(data)
+		if err == nil {
+			break
+		}
+		hook, err = convertDeploymentHook(data)
+		if err == nil {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
 	}
 	token, err := fn(hook)
 	if err != nil {
@@ -36,4 +63,40 @@ func (s *webhookService) Parse(req *http.Request, fn scm.SecretFunc) (scm.Webhoo
 		return hook, nil
 	}
 	return hook, nil
+}
+
+func convertPushHook(data []byte) (*scm.PushHook, error) {
+	hook := new(scm.PushHook)
+	err := json.Unmarshal(data, hook)
+	return hook, err
+}
+
+func converBranchHook(data []byte) (*scm.BranchHook, error) {
+	hook := new(scm.BranchHook)
+	err := json.Unmarshal(data, hook)
+	return hook, err
+}
+
+func convertCommentHook(data []byte) (*scm.IssueCommentHook, error) {
+	hook := new(scm.IssueCommentHook)
+	err := json.Unmarshal(data, hook)
+	return hook, err
+}
+
+func convertTagHook(data []byte) (*scm.TagHook, error) {
+	hook := new(scm.TagHook)
+	err := json.Unmarshal(data, hook)
+	return hook, err
+}
+
+func convertPullRequestHook(data []byte) (*scm.PullRequestHook, error) {
+	hook := new(scm.PullRequestHook)
+	err := json.Unmarshal(data, hook)
+	return hook, err
+}
+
+func convertDeploymentHook(data []byte) (*scm.DeployHook, error) {
+	hook := new(scm.DeployHook)
+	err := json.Unmarshal(data, hook)
+	return hook, err
 }
